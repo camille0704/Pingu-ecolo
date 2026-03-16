1d85c361010a0d1f83def5c5afcf8e6fc21e3861
loulcs <lou.lucas@efrei.net>
2026-03-16 18:15:29 +0100

    Fond

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ pygame.init()
 
 #fenetre du jeu
 pygame.display.set_caption("Pingu écolo !")
-pygame.display.set_mode((1080,720))
+screen = pygame.display.set_mode((1080,720))
 
+background =pygame.image.load('Pingu écolo! - assets/fond.jpg')
 running = True
 #tant que le jeu est actif
 while running:
-
+    #mettre arriere plan
+    screen.blit(background, (0,0))
+    #mettre a jour l'ecran
+    pygame.display.flip()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
